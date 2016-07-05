8b6e812e5a8042c9a7165aed0b0f055dfb7d8436
Ryan Burns <ryan.burns@clever.com>
2016-07-05 12:34:13 -0700

    Add check for empty AWS_REGION

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Work around boto3's lack of support for AWS_REGION
+if [ -z "$AWS_REGION" ] ; then
+    echo "Warning: AWS_REGION is not set. Defaulting to 'us-west-1'"
+    export AWS_REGION="us-west-1"
+fi
 mkdir -p ~/.aws
 echo "[default]" > ~/.aws/config
 echo "region=$AWS_REGION" >> ~/.aws/config
